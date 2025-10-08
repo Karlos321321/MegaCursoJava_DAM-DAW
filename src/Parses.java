9270366841d46c84e74f5e8ead17df3b25c60539
Karlos321321 <Karlos321321@gmail.com>
2025-10-09 00:45:14 +0200

    commit Parses

---
+++ src/Parses.java
@@ -0,0 +1,11 @@
+public class Parses {
+    public static void main(String[] args) {
+        
+        char num = '5';
+        int parsedNum = Integer.parseInt(String.valueOf(num));
+        System.out.println("Parsed integer: " + parsedNum);
+
+        
+    }
+
+}
