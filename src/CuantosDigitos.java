a72f362d0074190071db57c48817896d34a92816
Karlos321321 <Karlos321321@gmail.com>
2025-10-12 11:47:05 +0200

    commit

--- src/CuantosDigitos.java
+++ src/CuantosDigitos.java
@@ -6,14 +6,14 @@ public class CuantosDigitos {
         Scanner entrada = new Scanner(System.in);
         System.out.println("Introduce un numero: ");
         int numero = entrada.nextInt();
+       
         int cifras = 1;
 
         while ((numero = numero / 10) != 0) {
             cifras++;
         }   
         
-        System.out.println("El numero tiene " + cifras + (cifras==1 ? " digito" : " 1digitos"));
+        System.out.println("El numero tiene " + cifras + (cifras==1 ? " digito" : " digitos"));
         entrada.close();
-        System.out.println(9/10);
     }
 }
---
+++ src/Ejercicio3_Iniciacion.java
@@ -0,0 +1,12 @@
+public class Ejercicio3_Iniciacion {
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+
+        while (numero!=0) {
+            
+            System.out.println("Introduce un numero: ");
+            int numero = entrada.nextInt();
+        }
+
+}
