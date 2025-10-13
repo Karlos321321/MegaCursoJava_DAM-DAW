cd22a10f2373946b79a56ff0468c3aa2e2f9d14e
Karlos321321 <Karlos321321@gmail.com>
2025-10-13 20:21:17 +0200

    commit

--- src/Ejercicio3_Iniciacion.java
+++ src/Ejercicio3_Iniciacion.java
@@ -1,12 +1,31 @@
+import java.util.Scanner;
+
 public class Ejercicio3_Iniciacion {
     public static void main(String[] args) {
         
         Scanner entrada = new Scanner(System.in);
+        int numero = 1;
+        int sumaTotal=0;
+        String muestraSumaTotal="";
+        String numerosIntroducidos="";
 
         while (numero!=0) {
             
-            System.out.println("Introduce un numero: ");
-            int numero = entrada.nextInt();
+            System.out.println("Introduce un numero entero positivo ... : ");
+            numero = entrada.nextInt();
+
+            if (numero!=0) {
+                numerosIntroducidos += numero + ",";
+                muestraSumaTotal += numero + "+";
+            }
+           
+            if (numero<0) System.out.println("\nNO INTRODUZCAN NUMEROS NEGATIVOS !!!!!\n");
+                else sumaTotal += numero;
         }
 
+        System.out.println("Has introducido un 0, el programa ha terminado\n");
+        System.out.println("Numeros introducidos -->" + numerosIntroducidos + "\b ");
+        System.out.println("Suma Total --> " + muestraSumaTotal + "\b=" + sumaTotal);
+        entrada.close();
+    }
 }
---
+++ src/Ejercicio4_Iniciacion.java
@@ -0,0 +1,7 @@
+public class Ejercicio4_Iniciacion {
+    public static void main(String[] args) {
+        
+        
+    }
+
+}
