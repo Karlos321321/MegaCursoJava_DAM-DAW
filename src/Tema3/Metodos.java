59dd70a6af5b3937c931a78897829a1db3f0995a
Karlos321321 <Karlos321321@gmail.com>
2025-10-20 18:33:12 +0200

    commit metodos

---
+++ src/Tema3/Metodos.java
@@ -0,0 +1,38 @@
+package Tema3;
+
+public class Metodos {
+    public static void main(String[] args) {
+
+        int num1 = 12, num2 = 12;
+        int base = 5, altura = 10;
+
+        System.out.println("El area del rectangulo es: " + areaRectangulo(base, altura));
+
+        if (sumaCondicional(num1, num2) == 0) {
+            System.out.println("Los numeros no son iguales, el resultado es 0");
+            System.out.println("a vale " + num1 + " y b vale " + num2);
+        } else {
+            System.out.println("Los numeros son iguales, el resultado es: " + sumaCondicional(num1, num2));
+        }
+        
+    }
+
+    static int suma (int a, int b) {
+            return a + b ;
+    }
+
+    static int sumaCondicional (int a, int b) {
+        if (a == b) {
+            return a + b ;
+        } else {
+            return 0;
+        }
+    }
+
+    static int areaRectangulo (int base, int altura) {
+        return base * altura;
+    }
+
+
+
+}
