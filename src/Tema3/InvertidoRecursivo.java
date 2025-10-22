2e0f6c6ae734fe9c2765d8b811a2969d6b13c156
Karlos321321 <Karlos321321@gmail.com>
2025-10-22 18:51:38 +0200

    commit

--- src/Tema3/InvertidoRecursivo.java
+++ src/Tema3/InvertidoRecursivo.java
@@ -3,7 +3,25 @@ package Tema3;
 public class InvertidoRecursivo {
     public static void main(String[] args) {
         
-        System.out.println("Hola");
+        String numero = "123456789";
+        invertido(numero);
+
+        System.out.println("La cadena " + numero + " invertida es --> " + invertido(numero));
+
 
     }
+
+
+    static String invertido(String numero){
+
+        String cadenaInvertida="";
+        for (int i = numero.length()-1; i>=0 ; i--) {
+            cadenaInvertida += numero.charAt(i); 
+        
+        }
+
+        return cadenaInvertida;
+        
+    }
 }
+
