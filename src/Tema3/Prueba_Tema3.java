23e9d8dbecacb1ecb81a6ba2ae034ce618c0bc21
Karlos321321 <Karlos321321@gmail.com>
2025-10-22 09:57:01 +0200

    commit tem3

--- src/Tema3/Prueba_Tema3.java
+++ src/Tema3/Prueba_Tema3.java
@@ -1,17 +1,27 @@
 package Tema3;
 
 public class Prueba_Tema3 {
-
-    static int numero=5;
     public static void main(String[] args) {
 
-        int numero=10;
-        numero = cubo(numero);
-        System.out.println(numero);
-
+        int num = 3;
+        int res = proceso1(num);
+        System.out.println("El resultado es: " + res);
+          
     }
 
-    static int cubo(int n){
-        return n*n*n;
-    }   
+    static int proceso1(int n1){
+        int p1 = 1;
+        for (int i = 0; i < n1; i++) {
+            p1 += proceso2(i);
+        }
+        return p1;
+    } 
+    
+    static int proceso2(int n2){
+        int p2 = 1;
+        for (int i = 0; i < n2; i++) {
+            p2 += n2;
+        }
+        return p2;
+    }
 }
