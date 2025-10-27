eb71d50adca7c83c3c0b9a119546dcb7f4c499db
Karlos321321 <Karlos321321@gmail.com>
2025-10-27 21:54:33 +0100

    commit

---
+++ src/Tema4/MatrizMaxima.java
@@ -0,0 +1,59 @@
+package Tema4;
+
+public class MatrizMaxima {
+    public static void main(String[] args) {
+        
+        int[][] matriz1 = {
+            {1}, 
+            {8,3,23},
+            {8,12}};
+            
+        int[][] matriz2 = {
+            {4}, 
+            {6,10,11}, 
+            {11,6}};
+
+        if (matricesIguales(matriz1, matriz2)) impresionMatriz(matrizMaxima(matriz1, matriz2));
+            else System.out.println("Las matrices no son iguales y no se pueden comparar !!!!"); 
+    }
+    
+    static boolean matricesIguales(int[][]n , int[][]n2){
+        if (n.length == n2.length){
+            for (int i = 0; i < n.length; i++) {
+                if(n[i].length != n2[i].length) return false;                
+            }
+            return true;     
+        }
+        return false;
+    }
+
+    static int[][] matrizMaxima(int[][] n , int n2[][]){
+
+        int[][] maxima = new int [n.length][];
+
+        for (int i = 0; i < n.length; i++) {
+            maxima[i] = new int[n[i].length];
+        }
+
+        for (int i = 0; i < maxima.length; i++) {
+            for (int j = 0; j < maxima[i].length; j++) {
+                
+                if (n[i][j]>n2[i][j]) maxima[i][j]=n[i][j];
+                else maxima[i][j]=n2[i][j];
+            }
+        }
+
+        return maxima;
+    }
+
+    static void impresionMatriz(int[][] n){
+
+        for (int[] fila : n) {
+            for (int columna : fila) {
+
+                System.out.print(columna + " ");
+            }
+            System.out.println();
+        }
+    }
+}
--- src/Tema4/Pruebas.java
+++ src/Tema4/Pruebas.java
@@ -3,34 +3,53 @@ package Tema4;
 public class Pruebas {
     public static void main(String[] args) {
         
-        int[][] array1 = {
-            {1,2,5},
-            {6,7,8},
-            {10,0,4}       
-        };
+          int[][] matriz1 = {
+            {1}, 
+            {8,3,23},
+            {8,12}};
+            
+        int[][] matriz2 = {
+            {4,2}, 
+            {6,10,11,21}, 
+            {11,6,32},
+            {12,1}};
 
-        int[][] array2 = {
-            {5,1,4},
-            {7,3,11},
-            {11,12,13}
-        };
+        impresionMatriz(matrizMaxima(matriz1, matriz2));
+    }
+    
+    static int[][] matrizMaxima(int[][] n , int n2[][]){
 
-        int total1 = comprobarMatrices(array1);
-        int total2 = comprobarMatrices(array2);
+        int[][] maxima;
 
-        if(total1>total2) System.out.println("EL array1 es mayor que el array2, no se pueden comparar");
-        else if (total2>total1) System.out.println("EL array2 es mayor que el array1, no se pueden comparar");
-        else imprime();
+        if (n.length>n2.length) maxima = new int[n.length][];
+        else maxima = new int[n2.length][];
+        
+        for (int i = 0; i < maxima.length; i++) {
+
+            if(n[i].length>n2[i].length) maxima[i] = new int[n[i].length];
+            else maxima[i] = new int[n2[i].length];
+        }
+
+        for (int i = 0; i < maxima.length; i++) {
+            for (int j = 0; j < maxima[i].length; j++) {
+                
+                if (n[i][j]>n2[i][j]) maxima[i][j]=n[i][j];
+                else maxima[i][j]=n2[i][j];
+            }
+        }
+
+        return maxima;
     }
 
-    static int comprobarMatrices(int n[][]){
+    static void impresionMatriz(int[][] n){
 
-        int tamañoTotal=0;
-        if(n==null) return 0;
-        for (int i = 0; i < n.length; i++) {
-            if(n[i]==null) return 0;
-            else tamañoTotal += n[i].length;
+        for (int[] fila : n) {
+            for (int columna : fila) {
+
+                System.out.print(columna + " ");
+            }
+            System.out.println();
         }
-        return tamañoTotal;    
     }
 }
+
