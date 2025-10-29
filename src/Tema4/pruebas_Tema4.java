de8813d6e219617eca07222a42b80db884852cdd
Karlos321321 <Karlos321321@gmail.com>
2025-10-29 17:38:23 +0100

    commit

--- src/Tema4/pruebas_Tema4.java
+++ src/Tema4/pruebas_Tema4.java
@@ -3,14 +3,7 @@ package Tema4;
 public class pruebas_Tema4 {
     public static void main(String[] args) {
 
-        char caraterDiagonal = 88;
-        char guion = 45;
-
-        System.out.println(caraterDiagonal);
-        System.out.println(guion);
-
-        
-        
+        System.out.println("Esto es una prueba de sincronizacion !!!!!");
+           
     }
-
 }
