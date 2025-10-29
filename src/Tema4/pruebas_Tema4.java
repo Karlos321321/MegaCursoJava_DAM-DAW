f3d07ae0460b39e02658a8ed9b852ec609a75600
Karlos321321 <Karlos321321@gmail.com>
2025-10-29 20:34:01 +0100

    commit

--- src/Tema4/pruebas_Tema4.java
+++ src/Tema4/pruebas_Tema4.java
@@ -3,8 +3,27 @@ package Tema4;
 public class pruebas_Tema4 {
     public static void main(String[] args) {
 
-        char corazon ='\u0041';
-        System.out.println("hola " + corazon);
-           
+        String cadena = "Programacion en Java".toUpperCase();
+        String cadenaNueva="";
+
+        for (int i = 0; i < cadena.length(); i++) {
+            cadenaNueva += cadena.charAt(i) + " ";
+        }
+
+        System.out.print("\u2554");
+
+        for (int i=0 ; i<cadenaNueva.length(); i++){
+             System.out.print("\u2550");
+        }
+
+        System.out.println();
+        System.out.print("\u2551");
+        System.out.print("  ");
+        System.out.print(cadenaNueva);
+
+        System.out.println();
+        System.out.println("\u255A");
+
+         
     }
 }
